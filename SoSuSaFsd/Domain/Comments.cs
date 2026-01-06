9b8089a59e3b5db6128ec3a863e35a4d6d3e66a7
Dakshesh <nattamai28@gmail.com>
2026-01-06 10:49:42 +0800

    Added Comment Feature

--- SoSuSaFsd/Domain/Comments.cs
+++ SoSuSaFsd/Domain/Comments.cs
@@ -1,10 +1,16 @@
-﻿namespace SoSuSaFsd.Domain
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoSuSaFsd.Domain
 {
     public class Comments : BaseDomainModel
     {
         public string? Content { get; set; }
         public int PostID { get; set; }
-        public string UserID { get; set; }
+
+        //Navigation properties
+        public string UserID { get; set; } = string.Empty;
+        [ForeignKey("UserID")]
+        public virtual Users? User { get; set; }
         public int? ParentCommentID { get; set; }
     }
 }
